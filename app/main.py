af81d74251e64204cd326b2434332c258c06dbcd
Ankush <anksuhpatelflp@gmail.com>
2026-05-10 14:30:52 +0530

    removed bg remover

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
 from io import BytesIO
 from fastapi.middleware.cors import CORSMiddleware
 from app.downloader import extract_video
-from app.bg_remover import remove_background
 
 app = FastAPI()
 
@@ -22,14 +21,3 @@ class VideoRequest(BaseModel):
 @app.post("/extract")
 async def extract(data: VideoRequest):
     return extract_video(data.url)
-
-@app.post("/remove-bg")
-async def remove_bg(file: UploadFile = File(...)):
-    image_bytes = await file.read()
-
-    output_image = remove_background(image_bytes)
-
-    return StreamingResponse(
-        BytesIO(output_image),
-        media_type="image/png"
-    )
